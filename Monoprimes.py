59ecc3d060ab1a335371fd9c0df35998a54650ea
Zain Rasheed <zenrasheed@gmail.com>
2023-01-12 21:43:06 -0800

    Ive added some comments

--- Monoprimes.py
+++ Monoprimes.py
@@ -4,11 +4,12 @@ Created on Wed Jan 11 21:08:46 2023
 
 @author: 1
 """
-
+#This is my first code uploaded to GitHub
 monoprimeanswer = 0
 multiples = 0
 
-
+#This function checks whether a number is prime or not and returns True if it is prime
+#Returns false if it is not prime
 def is_it_prime (num):
     for f in range(2, int(num/2) +1):
         if num % f == 0:
